80e569e847538a8e022d6e18c53feee7ef65c396
Buck <coderbuck@163.com>
2020-07-10 12:21:15 +0800

    init6

--- app/src/main/java/me/buck/jitpack/MainActivity.kt
+++ app/src/main/java/me/buck/jitpack/MainActivity.kt
@@ -2,11 +2,16 @@ package me.buck.jitpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.buck.jitpack.libaar.AarUtils
+import me.buck.jitpack.libjar.JarUtils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AarUtils.hello()
+        JarUtils().hello()
     }
 }
